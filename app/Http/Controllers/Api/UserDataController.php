32678799e0a6a4b60b3f23bc4d404a298ccd12ae
vfrbatik <vfrbatik@gmail.com>
2024-05-14 12:04:13 +0700

    update: api: userDataUpdate

--- app/Http/Controllers/Api/UserDataController.php
+++ app/Http/Controllers/Api/UserDataController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserDataController extends Controller
 {
@@ -58,6 +59,61 @@ class UserDataController extends Controller
             'data' => [
                 'user' => $user
             ]
+        ], 200);
+    }
+
+    public function getUserAvatar(Request $request) {
+        
+    }
+
+    public function updateUserAvatar(Request $request, ){
+        // check if file exist
+        if(!$request->hasFile('imgAvatar')){
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Form input error.',
+                'data' => [
+                    'formError' => 'Image file not specified.'
+                ]
+            ], 422);
+        }
+
+        // check user
+        $user = $request->user(); 
+        if(!$user){ 
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'User not found.'
+            ], 400);
+        }
+
+        // try to move file
+        $fileExt = $request->file('imgAvatar')->getClientOriginalExtension();
+        $filenameHash = uniqid("avatar_".time());
+        $filenameWithExt = $filenameHash.'.'.$fileExt;
+
+        if(!$request->file('imgAvatar')->storeAs(env('PATH_USER_AVATAR'), $filenameWithExt)){
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Failed storing file.'
+            ], 400);
+        }
+
+        // save filepath to user
+        $user->avatar = $filenameWithExt;
+        if(!$user->save()){
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Failed saving user data.'
+            ], 400);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Avatar updated.',
+            'data' => [
+                'user' => $user->only(['name', 'email', 'avatar'])
+                ]
         ], 200);
     }
 }
--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
@@ -18,4 +19,5 @@ class Authenticate extends Middleware
             return route('login');
         }
     }
+ 
 }
